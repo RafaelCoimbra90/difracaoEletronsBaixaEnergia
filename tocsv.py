5771bdbe291776d98d21d9897c9cbe1e94e35fe6
RafaelCoimbra90 <rafaelcoimbra1990@gmail.com>
2022-08-13 12:50:21 -0300

    começo a adicionar o novo experimento

--- tocsv.py
+++ tocsv.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 ###''' Aqui eu uso o Pandas para fazer a leitura do arquivo .json '''###
-df = pd.read_json (r'curve.json')
+df = pd.read_json (r'curve2.json')
 ###''' Aqui eu uso a função .to_csv para criar um arquivo .csv '''###
-df.to_csv (r'curves.csv', index = False)
+df.to_csv (r'curves2.csv', index = False)
